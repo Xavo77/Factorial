ae53fb308aa53558ff82f6197917861e4f0618f2
Xavo77 <janedaz@hotmail.com>
2017-08-03 13:19:06 -0500

    Add files

---
+++ ConsoleFactorial/ConsoleFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial de un número");
+            Console.WriteLine("Número:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            float fac = 1;
+            for (int i = num; i > 0; i--)
+            {
+                fac = fac * i;
+            }
+            Console.WriteLine("El factorial es: " + fac);
+            Console.ReadLine();
+        }
+    }
+}
